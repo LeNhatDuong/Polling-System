ab6adbcd5a69c44261abedb6d4c0542a11f69db9
Duong Nguyen <duong.nguyen@eastagile.com>
2022-06-02 16:09:22 +0700

    Add index view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
 def index(request):
-    return HttpResponse("Youre at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
+def detail(resquest, question_id):
+    return HttpResponse("You are looking at question {0}".format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response%question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s."% question_id)
