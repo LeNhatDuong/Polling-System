501bf3f126f0ba43c83bbaa69000fc65db90cc79
Duong Nguyen <duong.nguyen@eastagile.com>
2022-06-03 08:27:41 +0700

    Add view details and others view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template("polls/index.html")
@@ -9,8 +11,9 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
-def detail(resquest, question_id):
-    return HttpResponse("You are looking at question {0}".format(question_id))
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
