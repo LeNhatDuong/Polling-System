6e17bf53820f9b08b89131bc5a7c21051980fea3
Duong Nguyen <duong.nguyen@eastagile.com>
2022-06-03 10:45:12 +0700

    Change to django generic view

--- polls/views.py
+++ polls/views.py
@@ -4,21 +4,22 @@ from django.template import loader
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return HttpResponse(template.render(context, request))
+from django.views import generic
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         'latest_question_list': latest_question_list
+#     }
+#     return HttpResponse(template.render(context, request))
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
-    return render(request, "polls/results.html", {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk = question_id)
+#     return render(request, "polls/results.html", {'question': question})
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -28,7 +29,7 @@ def vote(request, question_id):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html',{
             'question': question,
-            'error_message' : "You didnt slect a choice."
+            'error_message' : "You didnt select a choice."
         })
     else:
         selected_choice.votes += 1
@@ -37,4 +38,19 @@ def vote(request, question_id):
         # with a post request. This prevents data from being posted twice
         # if a user hits the back button.
         return HttpResponseRedirect(reverse("polls:result", args= (question.id,)))
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
