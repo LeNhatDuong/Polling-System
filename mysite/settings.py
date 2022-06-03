b6ef8c83c8b6f3781316fd89ddf8efd272f4fdf2
Duong Nguyen <duong.nguyen@eastagile.com>
2022-06-03 16:10:38 +0700

    Delete secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u7qyg&7keof^c9#n_!l8ri0221zz8pa2ojz+(=(6ssyc^)*n2)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
